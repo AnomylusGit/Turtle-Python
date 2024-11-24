d2a72bfb5dd563a64cf196b67208ef427e8bba09
AnomylusGit <urath.aarush13@gmail.com>
2024-11-24 13:49:48 +0200

    Star

---
+++ star.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+
+sc = turtle.Turtle()
+turtle.title("Turtle Usage")
+#star
+#triangle1
+sc.forward(100)
+
+sc.left(120)
+sc.forward(100)
+
+sc.left(120)
+sc.forward(100)
+sc.penup()
+sc.right(150)
+sc.forward(50)
+#triangle2
+sc.pendown()
+sc.right(90)
+sc.forward(100)
+
+sc.right(120)
+sc.forward(100)
+
+sc.right(120)
+sc.forward(100)
+turtle.done()
