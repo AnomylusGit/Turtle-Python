0bfeb238062cbfce424d63c85b09c57ca8e4ec08
AnomylusGit <urath.aarush13@gmail.com>
2024-11-24 13:38:18 +0200

    Polygons

---
+++ polygons.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Turtle()
+turtle.title("Polygons")
+# Triangle
+sc.forward(100)
+
+sc.left(120)
+sc.forward(100)
+
+sc.left(120)
+sc.forward(100)
+
+sc.forward(100)
+# Square
+sc.forward(100)
+sc.left(90)
+sc.forward(100)
+sc.left(90)
+sc.forward(100)
+sc.left(90)
+sc.forward(100)
+sc.left(90)
