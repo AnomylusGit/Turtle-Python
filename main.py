b0b83af40f20cfe80afa1eeb67fb58a47ccad675
AnomylusGit <urath.aarush13@gmail.com>
2024-11-24 13:30:50 +0200

    Turtle

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.Screen().bgcolor("green")
+sc=turtle.Screen()
+sc.setup(400,400)
+sc = turtle.Turtle()
+turtle.title("Turtle Usage")
+# Triangle
+sc.forward(100)
+
+sc.left(120)
+sc.forward(100)
+
+sc.left(120)
+sc.forward(100)
+
